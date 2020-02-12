281fa6ba9a6355111dd02cd1bc04f54800c06024
Joey Merino <jgamerino@gmail.com>
2020-02-12 13:51:30 -0500

    fixed bootstrap issue with header and footer, fixed font awesome issues, changed hyperlink color, centered content

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Developer Profile Generator
+# GitHub Profile Generator
 
 A command-line application that dynamically generates a PDF profile from a GitHub username. The application will be invoked with the following command:
 
@@ -28,7 +28,7 @@ Refer to the [design mockup](Prof-Gen.gif).
 
 When preparing a report for stakeholders, it is important to have up-to-date information about members of the development team. Rather than navigating to each team member's GitHub profile, a command-line application will allow for quick and easy generation of profiles in PDF format.
 
-## HMTL, CSS, Node.js, ES6+, AXIOS, Inquirer, Electron-html-to
+HMTL, CSS, Node.js, ES6+, AXIOS, Inquirer, Electron-html-to
 
-# Repo: https://github.com/darh0ky/profile-generator
-# Project URL: https://darh0ky.github.io/profile-generator/
+Repo: https://github.com/darh0ky/profile-generator
+Project URL: https://darh0ky.github.io/profile-generator/
--- index.js
+++ index.js
@@ -44,11 +44,11 @@ function init() {
 				}
 				//data changed to shortened variable names
 				data.username = username;
-				data.numOfRepo = res.data.public_repos;
+				data.repos = res.data.public_repos;
 				data.name = res.data.name
 				data.followers = res.data.followers;
 				data.following = res.data.following;
-				data.portPic = res.data.avatar_url;
+				data.profPic = res.data.avatar_url;
 				data.location = res.data.location;
 				data.blog = res.data.blog;
 				data.company = res.data.company
@@ -61,10 +61,10 @@ function init() {
 						data.stars += res.data[i].stargazers_count;
 					}
 					//setting up pdf data into a variable
-					let resumeHTML = generateHTML(data);
+					let convertToPDF = htmlToPDF(data);
 					//passing pdf variable into electron for conversion
 					conversion({
-						html: resumeHTML
+						html: convertToPDF
 					}, function(err, result) {
 						if (err) {
 							return console.error(err);
@@ -105,201 +105,238 @@ const colors = [{ // Green
   
 }];
 // Generates HTML based on data given 
-function generateHTML(data) {
+function htmlToPDF(data) {
 	return `<!DOCTYPE html>
   <html lang="en">
-     <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
-        <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
-        <title>GitHub Profile Generator</title>
-        <style>
-            @page {
-              margin: 0;
-            }
-           *,
-           *::after,
-           *::before {
-           box-sizing: border-box;
-           }
-           html, body {
-           padding: 0;
-           margin: 0;
-           }
-           html, body, .wrapper {
-           height: 100%;
-           }
-           .wrapper {
-             height: 500px;
-           background-color: ${colors[data.color].wrapperBackground};
-           padding-top: 100px;
-           }
-         
-         
-           body {
-           background-color: white;
-           -webkit-print-color-adjust: exact !important;
-           font-family: 'garamond', sans-serif;
-           }
-           main {
-           background-color: #E9EDEE;
-           height: auto;
-           padding-top: 30px;
-           }
-           h1, h2, h3, h4, h5, h6 {
-           font-family: 'garamond', serif;
-           margin: 0;
-           }
-           h1 {
-           font-size: 3em;
-           }
-           h2 {
-           font-size: 2.5em;
-           }
-           h3 {
-           font-size: 2em;
-           }
-           h4 {
-           font-size: 1.5em;
-           }
-           h5 {
-           font-size: 1.3em;
-           }
-           h6 {
-           font-size: 1.2em;
-           }
-           .photo-header {
-           position: relative;
-           margin: 0 auto;
-           margin-bottom: -50px;
-           display: flex;
-           justify-content: center;
-           flex-wrap: wrap;
-           background-color: ${colors[data.color].headerBackground};
-           color: ${colors[data.color].headerColor};
-           padding: 10px;
-           width: 95%;
-           border-radius: 6px;
-           }
-           .photo-header img {
-           width: 250px;
-           height: 250px;
-           border-radius: 50%;
-           object-fit: cover;
-           margin-top: -75px;
-           border: 6px solid ${colors[data.color].photoBorderColor};
-           box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
-         
-          }
-           .photo-header h1, .photo-header h2 {
-           width: 100%;
-           text-align: center;
-           }
-           .photo-header h1 {
-           margin-top: 10px;
-           }
-           .links-nav {
-           width: 100%;
-           text-align: center;
-           padding: 20px 0;
-           font-size: 1.5em;
-           }
-           .nav-link {
-           display: inline-block;
-           margin: 5px 10px;
-           }
-          
-           .container {
-             position: relative;
-           padding: 50px;
-           padding-left: 100px;
-           padding-right: 100px;
-           }
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
+          <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
   
-           .row {
-             display: flex;
-             flex-wrap: wrap;
-             justify-content: space-between;
-             margin-top: 20px;
-             margin-bottom: 20px;
-           }
   
-           .card {
-             padding: 20px;
-             border-radius: 6px;
-             background-color: ${colors[data.color].headerBackground};
-             color: ${colors[data.color].headerColor};
-             margin: 20px;
-             
-           }
-           
-           .col {
-           flex: 1;
-           text-align: center;
-           }
   
-           a, a:hover {
-           text-decoration: none;
-           color: inherit;
-           font-weight: bold;
-           }
-  
-           @media print { 
-            body { 
-              zoom: .75; 
-            } 
-           }
-        </style>
-  
-        <body>
-          <header>
-          <div class="wrapper">
-            <div class='photo-header'>
-              <img src="${data.portPic}"><br>
-              <h1>Hi!</h1>
-              <h2>My name is ${data.name}</h2>
-              <h3>Currently @ ${data.company}</h3>
-              <div class="links-nav">
-              <a class="nav-link" href="https://www.google.com/maps/place/${data.location.split(' ')[0]}+${data.location.split(' ')[1]}"><i class="fas fa-location-arrow"></i>&nbsp ${data.location}</a>
-              <a class="nav-link" href="https://github.com/${data.username}"><i class="fab fa-github"></i>&nbsp Github</a>
-              <a class="nav-link" href="${data.blog}"><i class="fab fa-blogger"></i>&nbsp blog</a>
-            </div>
-  
-            </div>
-          </header>
-  
-          <div class="container">
-          <h1> I build thing and teach people to code. </h1>
-            <div class="row">
-         
-            </div>
-            <div class="row">
-              <div class='col card'>
-                <h2>Public Repositories: </h1>
-                <h3>${data.numOfRepo}</>
-              </div>
-  
-              <div class="col card">
-                <h2>Followers:</h2>
-                <h3>${data.followers}</h3>
-              </div>
-            </div>
-  
-            <div class="row">
-              <div class="card col">
-                <h2>Stars:</h2>
-                <h3> ${data.stars}</h3>
-              </div>
-              <div class="card col">
-                <h2>Following:</h2>
-                <h3> ${data.followers}</h3>
-              </div>
-            </div>
-            <div class="wrapper2"></div>
+      <title>GitHub Profile Generator</title>
+      <style>
+      @page {
+        margin: 0;
+      }
+     *,
+     *::after,
+     *::before {
+     box-sizing: border-box;
+     }
 
+           .footer, header, html {
+            background-color: ${colors[data.color].wrapperBackground};
+
+      -webkit-print-color-adjust: exact !important;
+      font-family: 'garamond', sans-serif;
+      }
+  .footer {
+      position: fixed;
+      height: 20%;
+      bottom: 0;
+      width: 100%;
+      background-color: ${colors[data.color].wrapperBackground};
+      -webkit-print-color-adjust: exact !important;
+
+  }
+  
+  header {
+      padding-top: 70px;
+      
+  
+  }
+  .photo-header {
+      background-color: ${colors[data.color].headerBackground};
+      color: ${colors[data.color].headerColor};
+      height: 400px;
+      width: 80%;
+      margin: 0 auto;
+      margin-bottom: -80px;
+      border-radius: 8px;
+      display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding-bottom: 30px
+  
+  
+  }
+  
+  .photo-background {
+         background-color: ${colors[data.color].wrapperBackground};
+      padding-top: 100px;
+  
+  }
+
+  #quote {
+    padding: 30px;
+  }
+  
+  img {
+      width: 250px;
+      height: 250px;
+      border-radius: 50%;
+      object-fit: cover;
+      margin-top: -75px;
+      border: 6px solid ${colors[data.color].photoBorderColor};
+      box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
+    
+  }
+  
+  .main-card1 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  
+  
+  }
+  .main-card2 {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: row; 
+  }
+  .card-body {
+    width: 500px;
+    background-color: ${colors[data.color].headerBackground};
+    color: ${colors[data.color].headerColor};
+    
+  }
+  .links-nav {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: row;
+      color: ${colors[data.color].headerColor};
+      width: 100%;
+      padding: 20px 0;
+      font-size: 1.5em;
+
+
+  }
+
+  a, a:hover {
+    text-decoration: none;
+    color: inherit;
+    font-weight: bold;
+    }
+
+  
+  .card-text, .card-title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .card-text {
+    font-size: 2em;
+  }
+
+  .card-title {
+    font-size: 3em;
+  }
+
+  #company-text {
+    font-size: 1.5em;
+  }
+
+  
+
+  @media print { 
+    body { 
+      zoom: .75; 
+    } 
+
+  
+  
+      </style>
+  </head>
+  <body>
+     <div class="photo-background"> 
+          <div class="photo-header">
+             
+  
+                <img src="${data.profPic}"><br>
+                <h1>Hi!</h1>
+                <h3>My name is ${data.name}</h3>
+                <p class="text-center" id="company-text">Currently @ ${data.company}</p>
+  
+                <div class="links-nav">
+                <a class="nav-link" href="https://www.google.com/maps/place/${data.location.split(' ')[0]}+${data.location.split(' ')[1]}"><i class="fas fa-location-arrow"></i>&nbsp ${data.location}</a>
+                <a class="nav-link" href="https://github.com/${data.username}"><i class="fab fa-github"></i>&nbsp Github</a>
+                <a class="nav-link" href="${data.blog}"><i class="fab fa-blogger"></i>&nbsp blog</a>
+              </div>
+    
+  </div> 
+  </br>    
+   
+    
+  
+  
+  </div>
+  </br>
+  </br>
+  </br>
+  <h1 class="text-center" id="quote">I build thing and teach people to code.</h1>
+  
+     <div class="container">
+         
+  <div class="main-card1">
+      <div class="row">
+          <div class="col-sm-6 col-md-offset-2"">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Public Repositories:</h5>
+                <p class="card-text">${data.repos}</p>
+              
+              </div>
+            </div>
           </div>
+          
+          <div class="col-sm-6 col-md-offset-2"">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Followers:</h5>
+                <p class="card-text">${data.followers}</p>
+              
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </br>
+      <div class="main-card2">
+        
+          <div class="row">
+              <div class="col-sm-6 col-md-offset-2"">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Stars:</h5>
+                    <p class="card-text">${data.stars}</p>
+                  
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-offset-2"">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Following:</h5>
+                    <p class="card-text">${data.followers}</p>
+                  
+                  </div>
+                </div>
+              </div>
+            </div>
+    
+  
+  
+  <div class="footer"></div>
   </body>
+  </html>
         `
 };
